9855b539b02b55cc6b0715a2ceee3668cdd9499d
Daniel Yinanc <danielyinanc@users.noreply.github.com>
2016-06-30 10:37:31 -0400

    Fixed Link Reference for Docker Compose

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-commandScrBackend=.backendUrl=\"http://$BACKEND_PORT_8080_TCP_ADDR:8080\"
+commandScrBackend=.backendUrl=\"http://backend:8080\"
 
 cat conf/config.json | ./jq-linux64 $commandScrBackend > test.json
 
